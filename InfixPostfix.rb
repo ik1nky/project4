ee3f55edd0c8f223e008463bd1f48ece11d7560c
Michael Kinkema <kinkemam@mail.gvsu.edu>
2014-11-04 10:41:06 -0500

    Create methods isOperator, isOperand, isLefParen, isRightParen, stackPrecedence

--- InfixPostfix.rb
+++ InfixPostfix.rb
@@ -6,7 +6,8 @@ class InfixPostfix
   
   # converts the infix expression string to postfix expression and returns it
   def infixToPostfix(exprStr)
-
+	my_array = exprStr.split(" ") #Creates array from tokenized exprStr
+	
   end
 
   # evaluate the postfix string and returns the result
@@ -18,27 +19,51 @@ class InfixPostfix
 
   # returns true if the input is an operator and false otherwise
   def operator?(str)
-   
+  	case str
+	when '+','-','*','/','%','^','('
+		return true
+	else
+		return false
+	end 
   end
 
   # returns true if the input is an operand and false otherwise
   def operand?(str)
-
+	return false
   end
 
   # returns true if the input is a left parenthesis and false otherwise
   def leftParen?(str)
-
+	case str
+	when '('
+		return true
+	else
+		return false
+	end
   end
 
   # returns true if the input is a right parenthesis and false otherwise
   def rightParen?(str)
-
+	case str
+	when ')'
+		return true
+	else
+		return false
+	end
   end
 
   # returns the stack precedence of the input operator
   def stackPrecedence(operator)
-
+	case operator
+	when '+','-'
+		return 1
+	when '*','/','%'
+		return 2
+	when '^'
+		return 3
+	else
+		return -1
+	end
   end
 
   # returns the input precedence of the input operator
